e7f785bc2897237c602a61938eb7a9a9f68330c0
Josh Jacoby <joshuadjacoby@gmail.com>
2024-08-26 21:53:02 -0700

    add markdownlint

--- nvim/init.lua
+++ nvim/init.lua
@@ -541,6 +541,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'prettierd',
         'eslint_d',
+        'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
