3575a4287a716dd08f3075ebb92adf992dfd0f80
Josh Jacoby <Josh.Jacoby@sony.com>
2024-08-28 12:41:01 -0700

    add octo.nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,4 +1,15 @@
 return {
+  {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'ibhagwan/fzf-lua',
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      require('octo').setup { picker = 'fzf-lua' }
+    end,
+  },
   { -- Better TypeScript LSP integration
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
