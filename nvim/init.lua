a3685f3f6bbb237402cab4c5989192a792ec8029
Josh Jacoby <joshuadjacoby@gmail.com>
2024-09-06 12:16:24 -0700

    feat: Add Buffer management and workspace persistence (#3)
    * add barbar, setup with vim-workspace
    
    * add auto-session replace workspace, add barbar keymaps
    
    * add correct sessionoptions, save barbar order and pins, lualine
    
    * move auto-session higher in config
    
    * replace barbar with bufferline
    
    * disable pin for now, causes issues such as repin on restore when unpinned

--- nvim/init.lua
+++ nvim/init.lua
@@ -203,14 +203,15 @@ require('lazy').setup({
 
       -- Document existing key chains
       require('which-key').add {
+        { '<leader>b', group = '[B]uffer' },
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
         { '<leader>g', group = '[G]it Fugitive' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>w', group = '[W]orkspace' },
       }
     end,
   },
@@ -728,15 +729,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'joshdick/onedark.vim',
-    dependencies = { 'navarasu/onedark.nvim' },
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'onedark'
-
-      -- Set's the tabline colorscheme
-      vim.g.lightline = { colorscheme = 'onedark' }
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -11,6 +11,146 @@ return {
       require('octo').setup { picker = 'fzf-lua' }
     end,
   },
+  { -- Buffers in the tabline
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      require('bufferline').setup {}
+      local map = function(keys, func, desc)
+        vim.keymap.set('n', keys, func, { desc = '[B]uffer: ' .. desc })
+      end
+
+      -- Move to previous/next
+      map('<A-,>', '<Cmd>BufferLineCyclePrev<CR>', 'Previous')
+      map('<A-.>', '<Cmd>BufferLineCycleNext<CR>', 'Next')
+      -- Re-order to previous/next
+      map('<A-<>', '<Cmd>BufferLineMovePrev<CR>', 'Move Previous')
+      map('<A->>', '<Cmd>BufferLineMoveNext<CR>', 'Move Next')
+      -- Goto buffer in position...
+      map('<A-1>', '<Cmd>BufferLineGoToBuffer 1<CR>', 'Goto 1')
+      map('<A-2>', '<Cmd>BufferLineGoToBuffer 2<CR>', 'Goto 2')
+      map('<A-3>', '<Cmd>BufferLineGoToBuffer 3<CR>', 'Goto 3')
+      map('<A-4>', '<Cmd>BufferLineGoToBuffer 4<CR>', 'Goto 4')
+      map('<A-5>', '<Cmd>BufferLineGoToBuffer 5<CR>', 'Goto 5')
+      map('<A-6>', '<Cmd>BufferLineGoToBuffer 6<CR>', 'Goto 6')
+      map('<A-7>', '<Cmd>BufferLineGoToBuffer 7<CR>', 'Goto 7')
+      map('<A-8>', '<Cmd>BufferLineGoToBuffer 8<CR>', 'Goto 8')
+      map('<A-9>', '<Cmd>BufferLineGoToBuffer 9<CR>', 'Goto 9')
+      map('<A-0>', '<Cmd>BufferLineGoToBuffer -1<CR>', 'Goto Last')
+      -- Pin/unpin buffer
+      -- map('<A-p>', '<Cmd>BufferLineTogglePin<CR>', 'Pin')
+      -- Close buffer
+      map('<A-c>', '<Cmd>BufferLinePickClose<CR>', 'Close')
+      -- Close commands
+      map('<leader>bc', '<Cmd>BufferLineCloseOthers<CR>', '[C]lose Others')
+      --                 :BufferLineCloseLeft
+      --                 :BufferLineCloseRight
+      -- Magic buffer-picking mode
+      map('<C-p>', '<Cmd>BufferLinePick<CR>', 'Pick')
+      -- Sort automatically by...
+      map('<leader>bd', '<Cmd>BufferLineSortByDirectory<CR>', 'Order By [D]irectory')
+      map('<leader>be', '<Cmd>BufferLineSortByExtension<CR>', 'Order By [E]xtension')
+      map('<leader>bt', '<Cmd>BufferLineSortByTabs<CR>', 'Order By [T]abs')
+    end,
+  },
+  { -- Buffers in the tabline
+    'romgrk/barbar.nvim',
+    enabled = false,
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font }, -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    config = function()
+      require('barbar').setup {
+        animation = false,
+      }
+
+      local map = function(keys, func, desc)
+        vim.keymap.set('n', keys, func, { desc = '[B]uffer: ' .. desc })
+      end
+
+      -- Move to previous/next
+      map('<A-,>', '<Cmd>BufferPrevious<CR>', 'Previous')
+      map('<A-.>', '<Cmd>BufferNext<CR>', 'Next')
+      -- Re-order to previous/next
+      map('<A-<>', '<Cmd>BufferMovePrevious<CR>', 'Move Previous')
+      map('<A->>', '<Cmd>BufferMoveNext<CR>', 'Move Next')
+      -- Goto buffer in position...
+      map('<A-1>', '<Cmd>BufferGoto 1<CR>', 'Goto 1')
+      map('<A-2>', '<Cmd>BufferGoto 2<CR>', 'Goto 2')
+      map('<A-3>', '<Cmd>BufferGoto 3<CR>', 'Goto 3')
+      map('<A-4>', '<Cmd>BufferGoto 4<CR>', 'Goto 4')
+      map('<A-5>', '<Cmd>BufferGoto 5<CR>', 'Goto 5')
+      map('<A-6>', '<Cmd>BufferGoto 6<CR>', 'Goto 6')
+      map('<A-7>', '<Cmd>BufferGoto 7<CR>', 'Goto 7')
+      map('<A-8>', '<Cmd>BufferGoto 8<CR>', 'Goto 8')
+      map('<A-9>', '<Cmd>BufferGoto 9<CR>', 'Goto 9')
+      map('<A-0>', '<Cmd>BufferLast<CR>', 'Goto Last')
+      -- Pin/unpin buffer
+      map('<A-p>', '<Cmd>BufferPin<CR>', 'Pin')
+      -- Close buffer
+      map('<A-c>', '<Cmd>BufferClose<CR>', 'Close')
+      -- Restore buffer
+      map('<A-s-c>', '<Cmd>BufferRestore<CR>', 'Restore')
+      -- Wipeout buffer
+      --                 :BufferWipeout
+      -- Close commands
+      --                 :BufferCloseAllButCurrent
+      --                 :BufferCloseAllButPinned
+      --                 :BufferCloseAllButCurrentOrPinned
+      --                 :BufferCloseBuffersLeft
+      --                 :BufferCloseBuffersRight
+      -- Magic buffer-picking mode
+      map('<C-p>', '<Cmd>BufferPick<CR>', 'Pick')
+      -- Sort automatically by...
+      map('<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', 'Order By [B]uffer Number')
+      map('<leader>bn', '<Cmd>BufferOrderByName<CR>', 'Order By [N]ame')
+      map('<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', 'Order By [D]irectory')
+      map('<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', 'Order By [L]anguage')
+      map('<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', 'Order By [W]indow Number')
+
+      -- Other:
+      -- :BarbarEnable - enables barbar (enabled by default)
+      -- :BarbarDisable - very bad command, should never be used
+    end,
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  { -- Session
+    'rmagatti/auto-session',
+    dependencies = {
+      {
+        'tiagovla/scope.nvim',
+        config = true,
+      },
+    },
+    config = function()
+      vim.opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions,globals'
+      require('auto-session').setup {
+        suppressed_dirs = { '~/', '~/projects', '~/Downloads', '/' },
+
+        pre_save_cmds = {
+          'ScopeSaveState',
+        },
+
+        post_restore_cmds = {
+          'ScopeLoadState',
+        },
+      }
+    end,
+  },
+  {
+    'tiagovla/scope.nvim',
+    config = function()
+      require('scope').setup {}
+      vim.keymap.set('n', '<leader>bm', '<cmd>ScopeMoveBuf<CR>', { desc = '[B]uffer: Scope [M]ove' })
+    end,
+  },
   { -- Better TypeScript LSP integration
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
@@ -19,7 +159,15 @@ return {
   { -- Status Bar
     'nvim-lualine/lualine.nvim',
     dependencies = { { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font } },
-    opts = {},
+    opts = {
+      sections = {
+        lualine_c = {
+          function()
+            return require('auto-session.lib').current_session_name(true)
+          end,
+        },
+      },
+    },
   },
   { -- Fzf
     'ibhagwan/fzf-lua',
@@ -86,11 +234,4 @@ return {
     build = 'make install',
   },
   'ntpeters/vim-better-whitespace', -- highlights trailing whitespace
-  'itchyny/lightline.vim', -- Used to shorten filename in tabs
-  { -- Workspace
-    'thaerkh/vim-workspace',
-    config = function()
-      vim.keymap.set('n', '<leader>tw', '<cmd>ToggleWorkspace<CR>', { desc = '[T]oggle [W]orkspace' })
-    end,
-  },
 }
